e7ca12c0dcd9f4cf55bf96863c55bca563d4df12
Deezek <62142680+deezek@users.noreply.github.com>
2026-05-24 17:57:26 +0200

    Delete test.ts

--- test.ts
+++
@@ -1,4 +0,0 @@
-import tirageService from './src/services/tirage.service.js';
-
-console.log(tirageService.getNextTirageUTC().toISOString());
-console.log("le tableau", tirageService.drawRandomNumbers(5, 50));
